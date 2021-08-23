11aec1934f4e9817f2f8eed9d106d0d1aab69f32
jan.tiedemann <janmaxtiedemann@aol.com>
2021-08-23 15:37:44 +0200

    fix(workflow): now respects timelimit

--- pkg/metrics/workflows_cache.go
+++ pkg/metrics/workflows_cache.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 	"time"
@@ -18,35 +19,45 @@ var (
 // workflowCache - used for limit calls to github api
 func workflowCache() {
 	for {
-		ww := make(map[string]map[int64]github.Workflow)
-
-		for _, repo := range config.Github.Repositories.Value() {
-			r := strings.Split(repo, "/")
-			s := make(map[int64]github.Workflow)
-			opt := &github.ListOptions{PerPage: int(config.Github.PageSize)}
-
-			for {
-				resp, rr, err := client.Actions.ListWorkflows(context.Background(), r[0], r[1], opt)
-				if err != nil {
-					log.Printf("ListWorkflows error for %s: %s", repo, err.Error())
-					panic(err)
-				}
-				for _, w := range resp.Workflows {
-					s[*w.ID] = *w
-				}
-
-				if rr.NextPage == 0 {
-					break
-				}
-				opt.Page = rr.NextPage
-			}
-
-			ww[repo] = s
-
+		cache, err := buildWorkflowMap()
+		if err != nil {
+			log.Println(err)
+		} else {
+			workflows = cache
 		}
-
-		workflows = ww
 
 		time.Sleep(time.Duration(60) * time.Second)
 	}
+}
+
+func buildWorkflowMap() (map[string]map[int64]github.Workflow, error) {
+
+	ww := make(map[string]map[int64]github.Workflow)
+
+	for _, repo := range config.Github.Repositories.Value() {
+		r := strings.Split(repo, "/")
+		s := make(map[int64]github.Workflow)
+		opt := &github.ListOptions{PerPage: int(config.Github.PageSize)}
+
+		for {
+			resp, rr, err := client.Actions.ListWorkflows(context.Background(), r[0], r[1], opt)
+			if err != nil {
+				return nil, fmt.Errorf("ListWorkflows error for %s: %s", repo, err.Error())
+			}
+			for _, w := range resp.Workflows {
+				s[*w.ID] = *w
+			}
+
+			if rr.NextPage == 0 {
+				break
+			}
+			opt.Page = rr.NextPage
+		}
+
+		ww[repo] = s
+
+	}
+
+	return ww, nil
+
 }
